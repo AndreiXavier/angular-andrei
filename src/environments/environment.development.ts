5e1715d21ac8c9240b82c436a89e76dc1b1bb518
AndreiXavier <129068168+AndreiXavier@users.noreply.github.com>
2023-04-20 14:50:00 -0300

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-409d1',
+    appId: '1:431020435129:web:e7be3d376b023cfc455d68',
+    storageBucket: 'kanbanfire-409d1.appspot.com',
+    apiKey: 'AIzaSyBeCqf4H8loPg63QHj1UsOMOJJKy-ZbjmA',
+    authDomain: 'kanbanfire-409d1.firebaseapp.com',
+    messagingSenderId: '431020435129',
+    measurementId: 'G-TQYYFYMJ70',
+  },};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyBeCqf4H8loPg63QHj1UsOMOJJKy-ZbjmA",
+    authDomain: "kanbanfire-409d1.firebaseapp.com",
+    projectId: "kanbanfire-409d1",
+    storageBucket: "kanbanfire-409d1.appspot.com",
+    messagingSenderId: "431020435129",
+    appId: "1:431020435129:web:e7be3d376b023cfc455d68",
+    databaseURL: "https://console.firebase.google.com/project/undefined/firestore/data/",
+    measurementId: "G-TQYYFYMJ70"
+  }
+}
